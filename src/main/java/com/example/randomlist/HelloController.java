cf8968b5db9b3c7402899d98f185e6646f21f0ba
GeorgeShiue <georgeshiue1130@gmail.com>
2023-05-11 16:27:31 +0800

    C
    檔案路徑更改

--- src/main/java/com/example/randomlist/HelloController.java
+++ src/main/java/com/example/randomlist/HelloController.java
@@ -178,7 +178,7 @@ public class HelloController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        fileChooser.setInitialDirectory(new File("C:\\Javafx_homework\\RandomList\\src\\main\\java\\csvfile"));
+        fileChooser.setInitialDirectory(new File("C:"));
         name.setCellValueFactory(new PropertyValueFactory<>("name"));
         channel.setCellValueFactory(new PropertyValueFactory<>("channel"));
         duration.setCellValueFactory(new PropertyValueFactory<>("duration"));
