f35826055ef4f750f55c88d35ed45f7dd824deb7
kou-ming <t0937993806@gmail.com>
2023-05-11 15:56:34 +0800

    test
    test

--- src/main/java/com/example/randomlist/Test.java
+++ src/main/java/com/example/randomlist/Test.java
@@ -12,7 +12,7 @@ public class Test extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ed_Scene.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 600);
-        stage.setTitle("Hello!");
+        stage.setTitle("Test What you what");
         stage.setScene(scene);
         stage.show();
     }
