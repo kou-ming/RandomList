4a969ab57fa21cf36302e7ae354ff78d03c8f37b
GeorgeShiue <georgeshiue1130@gmail.com>
2023-05-17 22:49:01 +0800

    Pick10
    隨機取10首功能完成

--- src/main/java/com/example/randomlist/EditController.java
+++ src/main/java/com/example/randomlist/EditController.java
@@ -21,22 +21,18 @@ import java.util.Scanner;
 public class EditController {
     @FXML
     private Button bt_random;
+
+    @FXML
+    private Button bt_pick10;
+
+    @FXML
+    private Button chscene_main;
+
     //public ArrayList <Song> songlist = HelloController.songslist;
     public ObservableList<Song> songlist = HelloController.List;
 
     @FXML
     void random_playlist(MouseEvent event) {
-
-        System.out.println("wow");
-        for (int i = 0; i < songlist.size(); i++) {
-            System.out.println(songlist.get(i).getName());
-        }
-
-        System.out.println();
-        System.out.println("我是分隔線");
-        System.out.println();
-
-        //ArrayList <Song> temp = new ArrayList<Song>();
         ObservableList<Song> temp = FXCollections.observableArrayList();
         int num = songlist.size();
         Random random = new Random();
@@ -60,21 +56,19 @@ public class EditController {
         }
 
         for (int i = 0; i < temp.size(); i++) {
-            System.out.println(temp.get(i).getName());
+            //System.out.println(temp.get(i).getName());
         }
-//        songlist.remove(0);
-        for(int i = 0 ;i < songlist.size() ; i++){
-            songlist.remove(0);
+
+        int size = temp.size();
+        songlist.clear();
+        for(int i = 0 ;i < size ; i++){
             songlist.add(temp.get(i));
         }
-//        songlist = temp;
-//        System.out.println(songlist.get(0).getName());
-//        songlist.remove(0);
     }
 
     @FXML
     void chscene_main(ActionEvent event) throws IOException {
-        System.out.println("change");
+        //System.out.println("change");
         Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
@@ -82,4 +76,35 @@ public class EditController {
         stage.show();
     }
 
+    @FXML
+    void pick10_playlist(MouseEvent event) {
+        ObservableList<Song> temp = FXCollections.observableArrayList();
+        int num = songlist.size();
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("wow");
+            boolean repeat = false;
+            int rand_num = random.nextInt(num);
+            for (int i = 0; i < temp.size(); i++) {
+                if (temp.get(i).equals(songlist.get(rand_num))) {
+                    repeat = true;
+                    break;
+                }
+            }
+            if (!repeat) {
+                temp.add(songlist.get(rand_num));
+            }
+            if (temp.size() == 10) {
+                break;
+            }
+        }
+
+        int temp_size = temp.size();
+        songlist.clear();
+        for(int i = 0 ; i < temp_size ; i++){
+            songlist.add(temp.get(i));
+        }
+    }
 }
