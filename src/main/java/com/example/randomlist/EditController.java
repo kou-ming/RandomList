7079f9232771700d53c478982e653cea8b159bbc
GeorgeShiue <georgeshiue1130@gmail.com>
2023-05-17 15:57:10 +0800

    EditController-2
    新的Controller檔案

--- src/main/java/com/example/randomlist/EditController.java
+++ src/main/java/com/example/randomlist/EditController.java
@@ -5,13 +5,19 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 
+import java.util.ArrayList;
+
 public class EditController {
     @FXML
     private Button bt_random;
+    public ArrayList <Song> songlist = HelloController.songslist;
 
     @FXML
     void random_playlist(MouseEvent event) {
+
         System.out.println("wow");
+        System.out.println(songlist.get(0).getName());
+
     }
 
 }
